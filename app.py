5cc75e237fa71fa8ec36d17dc85604ff8cc18b64
Bardia <bardiagsz@gmail.com>
2026-09-24 13:08:08 +0200

    fixed linking problems between html/css and flask

--- app.py
+++ app.py
@@ -5,4 +5,24 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("login.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/forgot-password")
+def forgot_password():
+    return render_template("forgotPass.html")
